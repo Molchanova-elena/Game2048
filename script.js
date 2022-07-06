ca8a8c4dbc40cc3ce8c8b8e15c733003f1c86210
Molchanova-elena <kibina88@mail.ru>
2022-07-06 20:32:05 +0700

    1й коммит
    Перенесла в создание клеток в JS

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 "use strict";
 
+const divGrid= document.querySelector('.grid');
+
+for (let i = 1; i <= 16; i++) {
+    const divCell = document.createElement('div');
+    divCell.className = 'cell';
+    divGrid.append(divCell);
+
+}
